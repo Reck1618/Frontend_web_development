1d6cab5f96ab6b5fd7f923bb392d18836ef0aa9b
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2023-03-11 15:26:00 +0530

    adding navigation component

--- React/firstapp/src/App.js
+++ React/firstapp/src/App.js
@@ -1,17 +1,9 @@
+import Navigation from './components/Navigation';
 import './App.css';
-
-function Heading() {
-  return (
-  <h1>Hello World, This is an h1 heading.</h1>
-  )
-}
 
 function App() {
   return ( 
-    <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
-      <Heading /> 
-    </div> 
+    <Navigation />
   ); 
 } 
  
---
+++ React/firstapp/src/components/Navigation.js
@@ -0,0 +1,13 @@
+function Navigation(props) {
+    return (
+        <div>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navigation;
