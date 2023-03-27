bd6b754ac78326b6b04e78e186ac9c2a6ab0fdb2
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2023-03-27 19:20:17 +0530

    added signup form

--- React/firstapp/src/App.js
+++ React/firstapp/src/App.js
@@ -2,16 +2,19 @@ import AboutMe from './components/About';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
+import SignUp from './components/SignUp';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-    <Navigation first="Home" second="About" third="Contact" />
+    <Navigation first="Home" second="About" third="Contact" fourth="SignUp" />
     <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/aboutMe" element={<AboutMe />} />
+    <Route path="/signUp" element={<SignUp />} />
     </Routes>
     <Footer />
     </>
