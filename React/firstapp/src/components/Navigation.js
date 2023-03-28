202f66aff1b31509b35cad668e9fd9e4d798c909
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2023-03-28 22:46:50 +0530

    fix

--- React/firstapp/src/components/Navigation.js
+++ React/firstapp/src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation(props) {
                 <li><Link to="/" className="nav-item">{props.first}</Link></li>
                 <li><Link to="/aboutMe" className="nav-item">{props.second}</Link></li>
                 <li><Link to="#" className="nav-item">{props.third}</Link></li>
-                <li><Link to="/signUp" className="nav-item" color='#cd0a0a'>{props.fourth}</Link></li>
+                <li><Link to="/signUp" className="nav-item">{props.fourth}</Link></li>
             </ul>
         </nav>
     );
