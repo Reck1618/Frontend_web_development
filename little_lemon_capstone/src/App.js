795c5e0dd0116497bde423ee8ec54f4daa123931
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2023-08-02 15:22:38 +0530

    added uiux

--- little_lemon_capstone/src/App.js
+++ little_lemon_capstone/src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My React App</h1>
-      </div>
+      <>
+      </>
     );
   }
 }
